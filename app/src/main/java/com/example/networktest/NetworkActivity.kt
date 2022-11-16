8863a3b488eefaa59c5c89cd0e1b7735b9955e8d
rchen <15195866500@163.com>
2022-11-17 00:27:42 +0800

    handler

--- app/src/main/java/com/example/networktest/NetworkActivity.kt
+++ app/src/main/java/com/example/networktest/NetworkActivity.kt
@@ -1,8 +1,13 @@
 package com.example.networktest
 
+import android.content.Context
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.os.Message
 import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.networktest.databinding.ActivityNetworkBinding
@@ -10,16 +15,19 @@ import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import okhttp3.*
 import java.io.IOException
-import java.lang.Thread.currentThread
-import kotlin.concurrent.thread
+import java.nio.charset.Charset
 
 class NetworkActivity : AppCompatActivity() {
     val TAG = "NetworkActivity"
+    //public final val TAG: String = "NetworkActivity"
     private lateinit var binding: ActivityNetworkBinding
     lateinit var viewModel: NetworkViewModel
+    
+    private lateinit var context: Context
     //private lateinit var personList: ArrayList<PersonResponse>
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        context = this
         super.onCreate(savedInstanceState)
         //setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(NetworkViewModel::class.java)
@@ -46,6 +54,8 @@ class NetworkActivity : AppCompatActivity() {
         setContentView(binding.root)
         sendRequestWithOkHttp()
     }
+    
+    
 
 
     /*private fun sendRequestWithHttpURLConnection(){
@@ -76,17 +86,27 @@ class NetworkActivity : AppCompatActivity() {
     private fun sendRequestWithOkHttp(){
         Log.d("TAG","Send")
         val client = OkHttpClient()
-        val request = Request.Builder().url("https://www.jianshu.com/p/289dfb1a839a").build()
-        //val request = Request.Builder().url("https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture/users.json").build()
+        //val request = Request.Builder().url("https://www.jianshu.com/p/289dfb1a839a").build()
+        val request = Request.Builder().url("https://raw.staticdn.net/android10/Sample-Data/master/Android-CleanArchitecture/users.json").build()
         Log.d("TAG","request")
         client.newCall(request).enqueue(object :Callback{
+
             override fun onFailure(call: Call, e: IOException) {
                 //println("未能获取数据："+ Thread.currentThread().name)
                 Log.d("TAG","未能获取数据")
             }
 
             override fun onResponse(call: Call, response: Response) {
-                Log.d("TAG","成功获取数据")
+                var body = response.body
+                var bytes = body?.bytes()
+                var result = bytes?.toString(Charset.defaultCharset())
+                Log.d("TAG","成功获取数据"+result.toString())
+                if (result != null){
+                    //showResponse(responseData)
+                    //parseJSONWithJSONObject(responseData)
+                    parseJSONWithGSON(result)
+                    Log.d("TAG","request")
+                }
             }
         })
 
@@ -136,16 +156,55 @@ class NetworkActivity : AppCompatActivity() {
             e.printStackTrace()
         }
     }*/
+    private val listHandler by lazy { ListHandler(this,binding) }
+    class ListHandler(val context: Context,val binding: ActivityNetworkBinding):Handler(Looper.getMainLooper()){
+        override fun handleMessage(msg: Message) {
+            //when(msg)
+            val contactList = msg.obj as List<ListResponse>
+            Log.d("TAG","NetworkActivity:"+contactList)
+            val adapter = NetworkAdapter(context,contactList)
+            binding.recyclerView.adapter = adapter
+
+        }
+
+    }
 
     private fun parseJSONWithGSON(jsonData: String){
         val gson = Gson()
-        val typeOf = object : TypeToken<List<Person>>(){}.type
-        val personList = gson.fromJson<List<Person>>(jsonData, typeOf)
+        val typeOf = object : TypeToken<List<ListResponse>>(){}.type
+        val contactList = gson.fromJson<List<ListResponse>>(jsonData, typeOf)
         //personList = Gson().fromJson(jsonData,typeOf)
-        for (person in personList){
+        for (person in contactList){
             Log.d(TAG,"id is ${person.id}")
-            Log.d(TAG,"full_name is ${person.full_name}")
+            Log.d(TAG,"full_name is ${person.name}")
             Log.d(TAG,"followers is ${person.followers}")
         }
+
+        val msg = Message.obtain()
+        msg.what = 1
+        msg.obj = contactList
+        listHandler.sendMessage(msg)
+
+        //listHandler.sendEmptyMessage(1)
     }
 }
+/*
+
+Handler() {
+    send()
+    handleMessage()
+}
+
+Handler handler = new Handler() {
+    @override
+    HandlerMessage() {
+
+    }
+}
+
+secondThread()
+{
+    main() {
+        handler.send(new Message(1))
+    }
+}*/
