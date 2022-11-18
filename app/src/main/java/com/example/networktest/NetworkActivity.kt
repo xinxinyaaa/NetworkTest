6cba82812030cc209b6203f4979bb35c014fe68f
uliuj620 <jiaxin.liu03-ext.@yanfeng.com>
2022-11-18 15:29:27 +0800

    modify networkactivity

--- app/src/main/java/com/example/networktest/NetworkActivity.kt
+++ app/src/main/java/com/example/networktest/NetworkActivity.kt
@@ -23,25 +23,12 @@ class NetworkActivity : AppCompatActivity() {
     lateinit var viewModel: NetworkViewModel
 
     private lateinit var context: Context
-    //private lateinit var personList: ArrayList<PersonResponse>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         context = this
         super.onCreate(savedInstanceState)
-        //setContentView(R.layout.activity_main)
         viewModel = ViewModelProvider(this).get(NetworkViewModel::class.java)
         initData()
-        /*binding = ActivityNetworkBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        binding.recyclerView.layoutManager = LinearLayoutManager(this)
-        val adapter = NetworkAdapter(this,viewModel.contactList)
-        binding.recyclerView.adapter = adapter
-        setContentView(binding.root)
-        sendRequestWithOkHttp()*/
-        /*binding.sendRequestBtn.setOnClickListener {
-            //sendRequestWithHttpURLConnection()
-            sendRequestWithOkHttp()
-        }*/
     }
 
     fun initData(){
@@ -54,34 +41,6 @@ class NetworkActivity : AppCompatActivity() {
         sendRequestWithOkHttp()
     }
 
-
-
-
-    /*private fun sendRequestWithHttpURLConnection(){
-        //开启线程发起网络请求
-        thread {
-            var connection: HttpURLConnection? = null
-            try {
-                val response = StringBuilder()
-                val url = URL("https://www.baidu.com")
-                connection = url.openConnection() as HttpURLConnection
-                connection.connectTimeout = 8000
-                val input = connection.inputStream
-                //对获取到的输入流进行读取
-                val reader = BufferedReader(InputStreamReader(input))
-                reader.use {
-                    reader.forEachLine {
-                        response.append(it)
-                    }
-                }
-                showResponse(response.toString())
-            }catch (e:Exception){
-                e.printStackTrace()
-            }finally {
-                connection?.disconnect()
-            }
-        }
-    }*/
     private fun sendRequestWithOkHttp(){
         Log.d("TAG","Send")
         val client = OkHttpClient()
@@ -108,63 +67,17 @@ class NetworkActivity : AppCompatActivity() {
                 }
             }
         })
-
-
-        /*thread {
-            try {
-                val client = OkHttpClient()
-                //val request = Request.Builder().url("https://www.baidu.com").build()
-                val request = Request.Builder().url("https://raw.githubusercontent.com/android10/Sample-Data/master/Android-CleanArchitecture/users.json").build()
-                val response = client.newCall(request).execute()
-                val responseData = response.body?.string()
-                if (responseData != null){
-                    //showResponse(responseData)
-                    //parseJSONWithJSONObject(responseData)
-                    parseJSONWithGSON(responseData)
-                    Log.d("TAG","request")
-                }
-
-            }catch (e:Exception){
-                e.printStackTrace()
-            }
-        }*/
     }
 
-    /*private fun showResponse(response:String){
-        runOnUiThread {
-            //进行UI操作，显示结果在界面上
-            binding.responseText.text = response
 
-        }
-
-    }*/
-    /*private fun parseJSONWithJSONObject(jsonData: String){
-        try {
-            val jsonArray = JSONArray(jsonData)
-            for (i in 0 until jsonArray.length()){
-                val jsonObject = jsonArray.getJSONObject(i)
-                val id = jsonObject.getString("id")
-                val full_name = jsonObject.getString("full_name")
-                val followers = jsonObject.getString("followers")
-                Log.d("MainActivity","id is $id")
-                Log.d("MainActivity","full_name is $full_name")
-                Log.d("MainActivity","id is $followers")
-
-            }
-        }catch (e:Exception){
-            e.printStackTrace()
-        }
-    }*/
     private val listHandler by lazy { ListHandler(this,binding) }
     class ListHandler(val context: Context,val binding: ActivityNetworkBinding):Handler(Looper.getMainLooper()){
         override fun handleMessage(msg: Message) {
-            //when(msg)
             val contactList = msg.obj as List<Person>
             Log.d("TAG","NetworkActivity:"+contactList)
             val adapter = NetworkAdapter(context,contactList)
             binding.recyclerView.adapter = adapter
         }
-
     }
 
     private fun parseJSONWithGSON(jsonData: String){
@@ -182,7 +95,5 @@ class NetworkActivity : AppCompatActivity() {
         msg.what = 1
         msg.obj = contactList
         listHandler.sendMessage(msg)
-
-        //listHandler.sendEmptyMessage(1)
     }
 }
